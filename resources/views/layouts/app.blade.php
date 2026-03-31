54e7a01f15ef0ffca1ea763f5cd7db8d70e455df
Zaskia Azila <zaskiaazila385@gmail.com>
2026-03-31 22:23:42 +0700

    login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
     <title>Manajemen Perpustakaan</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -18,17 +17,24 @@
         </button>
         
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link {{ request()->is('/') ? 'active' : '' }}" href="{{ route('home') }}">Home</a>
+                    <a class="nav-link {{ request()->is('home') ? 'active' : '' }}" href="{{ route('home') }}">Home</a>
                 </li>
-                
                 <li class="nav-item">
                     <a class="nav-link {{ request()->is('books*') ? 'active' : '' }}" href="{{ route('books.index') }}">Data Buku</a>
                 </li>
-                
                 <li class="nav-item">
                     <a class="nav-link {{ request()->is('categories*') ? 'active' : '' }}" href="{{ route('categories.index') }}">Data Kategori</a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button type="submit" class="btn btn-danger btn-sm mt-1">Logout</button>
+                    </form>
                 </li>
             </ul>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,28 @@
 <?php
-
+ 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
-use App\Http\Controllers\CategoryController;
-use App\Models\Book;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController; 
+use App\Models\Book; 
+ 
 
+Route::get('/', [AuthController::class, 'showLogin'])->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+ 
 
-// Route::get('/', function () {
-//     return redirect()->route('books.index');
-// });
-Route::get('/', function () {
-    $books = Book::latest()->get(); 
-    return view('home', compact('books'));
-})->name('home');
-
-
-Route::resource('books', BookController::class);
-Route::resource('categories', CategoryController::class);
+Route::middleware('auth')->group(function () {
+   
+    Route::get('/home', function () {
+        
+        $books = Book::latest()->get(); 
+        return view('home', compact('books'));
+    })->name('home');
+ 
+    Route::resource('books', BookController::class);
+    
+   
+    Route::resource('categories', CategoryController::class);
+   
+});
